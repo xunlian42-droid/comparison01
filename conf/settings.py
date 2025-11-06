5487a14e66df1eb9c590d3c8d8c28bffc1d45c1c
xunlian42-droid <xunlian42@gmail.com>
2025-11-06 12:01:00 +0900

    Djangoプロジェクトを更新

--- conf/settings.py
+++ conf/settings.py
@@ -155,6 +155,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ← 追加
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
